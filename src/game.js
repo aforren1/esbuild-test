3ffebfaeb693ed2c29ba51b5e1280b3c4905098f
Alex Forrence <alex.forrence@gmail.com>
2021-09-03 11:13:37 -0400

    eslint clarification

--- src/game.js
+++ src/game.js
@@ -39,51 +39,51 @@ const phaser_config = {
 
 window.addEventListener('load', () => {
 
-  const game = new Phaser.Game(phaser_config),
-    // TODO: figure out prolific/mturk/elsewhere here (URL parsing)
-    // Remember that localStorage *only stores strings*
-    url_params = new URL(window.location.href).searchParams,
-    // If coming from prolific, use that ID. Otherwise, generate some random chars
-    randomString = (length) =>
-      [...Array(length)].
-        map(() => (~~(Math.random() * 36)).toString(36)).
-        join('')
+  const game = new Phaser.Game(phaser_config)
+  // TODO: figure out prolific/mturk/elsewhere here (URL parsing)
+  // Remember that localStorage *only stores strings*
+  const url_params = new URL(window.location.href).searchParams
+  // If coming from prolific, use that ID. Otherwise, generate some random chars
+  const randomString = (length) =>
+    [...Array(length)].
+      map(() => (~~(Math.random() * 36)).toString(36)).
+      join('')
   let id =
     url_params.get('SONA_ID') ||
     url_params.get('PROLIFIC_PID') ||
     url_params.get('id') ||
-    randomString(8),
-    reference_angle = 270,
-    // rotation or clamp, default to clamp
-    manipulation = url_params.get('manipulation') || 'clamp'
+    randomString(8)
+  let reference_angle = 270
+  // rotation or clamp, default to clamp
+  let manipulation = url_params.get('manipulation') || 'clamp'
   if (!['rotation', 'clamp'].includes(manipulation)) {
 
     console.error('Manipulation not found.')
 
   }
-  let ua_res = new UAParser().getResult(),
-    user_config = {
-      id: id.slice(0, 8), // just the first part of the ID, we don't need to store the whole thing
-      is_prolific: url_params.get('PROLIFIC_PID') !== null,
-      is_sona: url_params.get('SONA_ID') !== null,
-      institution: 'yale',
-      description: 'consciousness clamp v1',
-      datetime: new Date(),
-      already_visited: localStorage.getItem('conscious-clamp') !== null,
-      manipulation: manipulation,
-      width: game.config.width,
-      height: game.config.height,
-      renderer: game.config.renderType === Phaser.CANVAS ? 'canvas' : 'webgl',
-      // only take a subset of the UA results-- we don't need everything
-      user_agent: {
-        browser: ua_res.browser,
-        os: ua_res.os
-      },
-      fullscreen_supported: document.fullscreenEnabled, // this is pretty important for us?
-      debug: url_params.get('debug') !== null,
-      version: 1,
-      reference_angle: reference_angle
-    }
+  let ua_res = new UAParser().getResult()
+  let user_config = {
+    id: id.slice(0, 8), // just the first part of the ID, we don't need to store the whole thing
+    is_prolific: url_params.get('PROLIFIC_PID') !== null,
+    is_sona: url_params.get('SONA_ID') !== null,
+    institution: 'yale',
+    description: 'consciousness clamp v1',
+    datetime: new Date(),
+    already_visited: localStorage.getItem('conscious-clamp') !== null,
+    manipulation: manipulation,
+    width: game.config.width,
+    height: game.config.height,
+    renderer: game.config.renderType === Phaser.CANVAS ? 'canvas' : 'webgl',
+    // only take a subset of the UA results-- we don't need everything
+    user_agent: {
+      browser: ua_res.browser,
+      os: ua_res.os
+    },
+    fullscreen_supported: document.fullscreenEnabled, // this is pretty important for us?
+    debug: url_params.get('debug') !== null,
+    version: 1,
+    reference_angle: reference_angle
+  }
   game.user_config = user_config // patch in to pass into game
   // set up for user
   localStorage.setItem('conscious-clamp', 1)
